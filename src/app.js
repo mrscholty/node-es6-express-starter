4cdf46b97266eaf567038bc3a1d8e163449c0b3c
Markus Scholtysik <mrscholtysik@gmail.com>
2017-10-22 23:45:02 +0200

    changed domain (fruits --> programming languages) added routes for get one, post and delete

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
 import express from 'express';
-import fruitsRoute from './routes/fruits.route';
+import bodyParser from 'body-parser';
+import languagesRoute from './routes/languages.route';
 
 let app = express();
 
-app.use('/api', fruitsRoute);
+// configure express middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
+// enable CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+// use routes
+app.use('/api', languagesRoute);
+
+// start server
 app.listen(3000, () => {
-   console.log('API listens ...');
+    console.log('API listens ...');
 });
 
 
---
+++ src/routes/languages.route.js
@@ -0,0 +1,39 @@
+import {Router} from 'express';
+
+const router = Router();
+
+let languages = [
+    {id: '1', name: 'JavaScript'},
+    {id: '2', name: 'Java'},
+    {id: '3', name: 'C#'},
+    {id: '4', name: 'Pascal'},
+    {id: '5', name: 'C'}
+];
+
+router.get('/', (req, res) => {
+    res.json(languages);
+});
+
+router.get('/:id', (req, res) => {
+    let id = req.params.id;
+    res.json(languages.find(item => item.id === id) || {});
+});
+
+router.post('/', (req, res) => {
+   let body = req.body;
+   languages.push(body);
+   res.json(body);
+});
+
+router.delete('/:id', (req, res) => {
+    let id = req.params.id;
+    let itemIndex  = languages.findIndex(item => item.id === id);
+    let item = languages[itemIndex] || {};
+    languages = [
+        ...languages.slice(0, itemIndex),
+        ...languages.slice(itemIndex + 1)
+    ];
+    res.json(item);
+});
+
+export default router;
