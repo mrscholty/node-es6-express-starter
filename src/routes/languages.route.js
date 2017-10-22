ded4eeef6f4c7bae3365fb9fb1a7c2b1830865d8
Markus Scholtysik <mrscholtysik@gmail.com>
2017-10-23 00:16:32 +0200

    added put route changed post route implemention added "stage-2" babel preset

--- src/routes/languages.route.js
+++ src/routes/languages.route.js
@@ -20,14 +20,23 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-   let body = req.body;
-   languages.push(body);
-   res.json(body);
+    let body = req.body;
+    languages = [...languages, body];
+    res.json(body);
+});
+
+router.put('/:id', (req, res) => {
+    let id = req.params.id;
+    let body = req.body;
+    languages = languages.map(item => item.id === id ?
+        {...item, name: body.name} : item
+    );
+    res.json(languages.find(item => item.id === id) || {});
 });
 
 router.delete('/:id', (req, res) => {
     let id = req.params.id;
-    let itemIndex  = languages.findIndex(item => item.id === id);
+    let itemIndex = languages.findIndex(item => item.id === id);
     let item = languages[itemIndex] || {};
     languages = [
         ...languages.slice(0, itemIndex),
